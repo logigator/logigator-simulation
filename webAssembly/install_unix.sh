127e5d2ca3b24623666bcf82c513039268d48e7a
Andreas <andreas.sch4@gmail.com>
2019-10-08 23:49:10 +0200

    make my friend travis even happier

--- webAssembly/install_unix.sh
+++ webAssembly/install_unix.sh
@@ -4,7 +4,7 @@ if [ -f 'webAssembly/dist/logigator-simulation.wasm' ]; then
   exit 0
 fi
 
-if [ -d 'webAssembly/emscripten' ]; then
+if [ ! -d 'webAssembly/emscripten' ]; then
   git clone https://github.com/emscripten-core/emsdk.git webAssembly/emscripten || exit 1
   bash webAssembly/emscripten/emsdk install latest
 fi
