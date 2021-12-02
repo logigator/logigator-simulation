a85bd7ce898330630d7bfc39904f76ec9b065f5a
Andreas Schlößl <andreas.sch4@gmail.com>
2021-12-02 20:16:21 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,40 @@
 export declare type Board = {
-    links: number;
-    components: {
-        type: ComponentType;
-        inputs: number[];
-        outputs: number[];
-    }[];
+	links: number;
+	components: {
+		type: ComponentType;
+		inputs: number[];
+		outputs: number[];
+	}[];
 }
 
 export declare type BoardStatus = {
-    currentSpeed: number;
-    currentState: number;
-    threadCount: number;
-    componentCount: number;
-    linkCount: number;
-    tick: number;
+	currentSpeed: number;
+	currentState: number;
+	threadCount: number;
+	componentCount: number;
+	linkCount: number;
+	tick: number;
 }
 
 export declare type BoardState = {
-    components: boolean[][];
-    links: boolean[];
+	components: boolean[][];
+	links: boolean[];
 }
 
 export declare const enum ComponentType {
-    NOT = 1,
-    AND = 2,
-    OR = 3,
-    XOR = 4,
-    DELAY = 5,
-    CLK = 6,
+	NOT = 1,
+	AND = 2,
+	OR = 3,
+	XOR = 4,
+	DELAY = 5,
+	CLK = 6,
 
-    HalfAdder = 10,
-    FullAdder = 11,
-    ROM = 12,
-    D_FF = 13,
-    JK_FF = 14,
-    SR_FF = 15,
+	HalfAdder = 10,
+	FullAdder = 11,
+	ROM = 12,
+	D_FF = 13,
+	JK_FF = 14,
+	SR_FF = 15,
 	RNG = 16,
 	RAM = 17,
 	DECODER = 18,
@@ -42,22 +42,22 @@ export declare const enum ComponentType {
 	MUX = 20,
 	DEMUX = 21,
 
-    UserInput = 200,
+	UserInput = 200,
 	LED_Matrix = 204
 }
 
 export declare const enum InputEvent {
-    Cont,
-    Pulse
+	Cont,
+	Pulse
 }
 
 export declare module logicsim {
-    function init(board: Board): void;
-    function destroy(): void;
-    function start(threads?: number, ticks?: number, ms?: number, synchronized?: boolean): void;
-    function stop(): void;
-    function getStatus(): BoardStatus;
-    function getBoard(): BoardState;
-    function getLinks(): boolean[];
-    function triggerInput(componentIndex: number, inputEvent: InputEvent, state: boolean[]): void;
+	function init(board: Board): void;
+	function destroy(): void;
+	function start(threads?: number, ticks?: number, ms?: number, synchronized?: boolean): void;
+	function stop(): void;
+	function getStatus(): BoardStatus;
+	function getBoard(): BoardState;
+	function getLinks(): boolean[];
+	function triggerInput(componentIndex: number, inputEvent: InputEvent, state: boolean[]): void;
 }
